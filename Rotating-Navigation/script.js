9eac9cc74cf6174901624bb9778a4b35228a951c
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-12-16 12:03:56 -0800

    Rotating-Navigation: Finished css, including a couple new util classes for font styling, wrote js listeners for nav reveal. Done.

---
+++ Rotating-Navigation/script.js
@@ -0,0 +1,7 @@
+/* Nav Functionality */
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+
+open.addEventListener('click', () => container.classList.add('showNav'));
+close.addEventListener('click', () => container.classList.remove('showNav'));
