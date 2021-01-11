8c33189cf028e1130d6f4937479e2609e45b42eb
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-01-11 12:28:36 -0800

    Drawing-App: script can now draw

--- Drawing-App/script.js
+++ Drawing-App/script.js
@@ -1,10 +1,36 @@
 // dom - canvas and context
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-// vars from user input
+// vars for user input
 let size = 20;
+let isPressed = false;
 let color = 'black';
 let x, y;
+
+// mouse events - is mouse clicked, and where?
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+});
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+});
+canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+    // draw circle & line, creates rounded end
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    // reset x & y to current mousemove positions,
+    // create proper curved line
+    x = x2;
+    y = y2;
+  }
+});
 
 const drawCircle = (x, y) => {
   ctx.beginPath();
@@ -19,6 +45,6 @@ const drawLine = (x1, y1, x2, y2) => {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 };
